3b943dd215888fb1a9195aa219a21ddb97619267
Paul Ward <asmodai@gmail.com>
2012-09-05 16:49:26 +0100

    Oops, put wrong colour in custom file :)

--- custom/custom-24.el
+++ custom/custom-24.el
@@ -51,4 +51,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:background "#0e0412" :foreground "#0e0412")))))
+ '(default ((t (:background "#0e0412" :foreground "#c4b1cb")))))
