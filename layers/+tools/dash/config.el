8fe4a8b9a91b22d4bcb50d683443e0984e1c981c
Paul Ward <pward@alertlogic.com>
2015-12-05 16:07:14 +0000

    Added more changes.

---
+++ layers/+tools/dash/config.el
@@ -0,0 +1,43 @@
+;;; -*- Mode: Emacs-Lisp -*-
+;;;
+;;; config.el --- Dash configuration.
+;;;
+;;; Time-stamp: <>
+;;; Revision:   0
+;;;
+;;; Copyright (c) 2015 Paul Ward <pward@alertlogic.com>
+;;;
+;;; Author:     Paul Ward <pward@alertlogic.com>
+;;; Maintainer: Paul Ward <pward@alertlogic.com>
+;;; Created:    05 Dec 2015 15:28:14
+;;; Keywords:   
+;;; URL:        not distributed yet
+;;;
+;;;{{{ License:
+;;;
+;;; This program is free software: you can redistribute it
+;;; and/or modify it under the terms of the GNU General Public
+;;; Licenseas published by the Free Software Foundation,
+;;; either version 3 of the License, or (at your option) any
+;;; later version.
+;;;
+;;; This program isdistributed in the hope that it will be
+;;; useful, but WITHOUT ANY  WARRANTY; without even the implied
+;;; warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+;;; PURPOSE.  See the GNU General Public License for more
+;;; details.
+;;;
+;;; You should have received a copy of the GNU General Public
+;;; License along with this program.  If not, see
+;;; <http://www.gnu.org/licenses/>.
+;;;
+;;;}}}
+;;;{{{ Commentary:
+;;;
+;;;}}}
+
+(defvar *dash-helm-dash-docset-path*
+  "/Users/pward/Library/Application Support/Dash/DocSets/"
+  "Path containing Dash docsets.")
+
+;;; config.el ends here
