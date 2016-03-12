c548bd8c214783fd2443084a01ea763232aa04f3
Paul Ward <pward@alertlogic.com>
2016-03-12 05:05:14 +0000

    Changed email address.

--- layers/+lang/perl/packages.el
+++ layers/+lang/perl/packages.el
@@ -2,10 +2,10 @@
 ;;;
 ;;; packages.el --- Perl packages.
 ;;;
-;;; Copyright (c) 2016 Paul Ward <pward@alertlogic.com>
+;;; Copyright (c) 2016 Paul Ward <asmodai@gmail.com>
 ;;;
-;;; Author:     Paul Ward <pward@alertlogic.com>
-;;; Maintainer: Paul Ward <pward@alertlogic.com>
+;;; Author:     Paul Ward <asmodai@gmail.com>
+;;; Maintainer: Paul Ward <asmodai@gmail.com>
 ;;; Created:    11 Mar 2016 23:53:41
 ;;; Keywords:   
 ;;; URL:        not distributed yet
