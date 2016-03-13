0ad277b1292438f6cfc24c990490e5ae2c894b04
Paul Ward <pward@alertlogic.com>
2016-03-13 19:27:40 +0000

    Added support for SaltStack and Docker.

---
+++ layers/+tools/salt/packages.el
@@ -0,0 +1,42 @@
+;;; -*- Mode: Emacs-Lisp -*-
+;;;
+;;; packages.el --- SaltStack packages.
+;;;
+;;; Copyright (c) 2016 Paul Ward <asmodai@gmail.com>
+;;;
+;;; Author:     Paul Ward <asmodai@gmail.com>
+;;; Maintainer: Paul Ward <asmodai@gmail.com>
+;;; Created:    13 Mar 2016 19:24:16
+;;; Keywords:   
+;;; URL:        not distributed yet
+;;;
+;;;{{{ License:
+;;;
+;;; This program is free software: you can redistribute it
+;;; and/or modify it under the terms of the GNU General Public
+;;; Licenseas published by the Free Software Foundation,
+;;; either version 3 of the License, or (at your option) any
+;;; later version.
+;;;
+;;; This program is distributed in the hope that it will be
+;;; useful, but WITHOUT ANY  WARRANTY; without even the implied
+;;; warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+;;; PURPOSE.  See the GNU General Public License for more
+;;; details.
+;;;
+;;; You should have received a copy of the GNU General Public
+;;; License along with this program.  If not, see
+;;; <http://www.gnu.org/licenses/>.
+;;;
+;;;}}}
+;;;{{{ Commentary:
+;;;
+;;;}}}
+
+(setq salt-packages '((salt-mode)))
+
+(defun salt:init-salt-mode ()
+  (use-package salt-mode
+    :defer t))
+
+;;; packages.el ends here
