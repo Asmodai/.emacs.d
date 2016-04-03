d0722a59a340ee8056c80b358fe7ab7a6ea396ce
Paul Ward <pward@alertlogic.com>
2016-04-03 02:32:38 +0100

    Added support for the Go language.

---
+++ layers/+lang/go/config.el
@@ -0,0 +1,43 @@
+;;; -*- Mode: Emacs-Lisp -*-
+;;;
+;;; config.el --- Go layer configuration.
+;;;
+;;; Copyright (c) 2016 Paul Ward <pward@alertlogic.com>
+;;;
+;;; Author:     Paul Ward <pward@alertlogic.com>
+;;; Maintainer: Paul Ward <pward@alertlogic.com>
+;;; Created:    03 Apr 2016 02:15:11
+;;; Keywords:   
+;;; URL:        not distributed yet
+;;;
+;;;{{{ License:
+;;;
+;;; This program is free software: you can redistribute it
+;;; and/or modify it under the terms of the GNU General Public
+;;; Licenseas published by the Free Software Foundation,
+;;; either version 3 of the License, or (at your option) any
+;;; later version.
+;;;
+;;; This program is distributed in the hope that it will be
+;;; useful, but WITHOUT ANY  WARRANTY; without even the implied
+;;; warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+;;; PURPOSE.  See the GNU General Public License for more
+;;; details.
+;;;
+;;; You should have received a copy of the GNU General Public
+;;; License along with this program.  If not, see
+;;; <http://www.gnu.org/licenses/>.
+;;;
+;;;}}}
+;;;{{{ Commentary:
+;;;
+;;;}}}
+
+(bootstrap:defvar-company-backends go-mode)
+
+(defvar *go-use-gockech-for-testing* nil
+  "If using gocheck for testing when running the tests -check.f will be used
+instead of -run to specify the test that will be ran. Gocheck is mandatory for
+testing suites.")
+
+;;; config.el ends here
