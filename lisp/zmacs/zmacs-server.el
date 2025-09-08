ef558c46d100b4ec3cd0a9af24b5b02217dbe455
Paul Ward <paul@lisphacker.uk>
2025-09-08 08:30:31 +0100

    Improve `emacsclient` support.

--- lisp/zmacs/zmacs-server.el
+++ lisp/zmacs/zmacs-server.el
@@ -27,6 +27,7 @@
 ;;
 
 ;;; Code:
+;;;; Requirements:
 
 (require 'cl-lib)
 (require 'zlisp-platform)
@@ -43,7 +44,7 @@
   (or (server-running-p)
       (server-start)))
 
-;;;; Functions
+;;;; Server shutdown:
 
 (defun zmacs-server-shutdown ()
   "Kill all Emacs clients and the server."
@@ -52,23 +53,29 @@
       (save-buffers-kill-emacs)
     (message "Not running as daemon")))
 
-(defun zmacs/new-frame-file-or-dashboard ()
-  "Show dashboard if FRAME is `*scratch*'."
-  (if (= (zmacs/server-gui-client-count) 1)
-      (zlisp/initial-frame-size)
-    (zlisp/initial-server-frame-size))
-  (zmacs-line-clockface-update-fontset "ClockFaceRectSolid")
-  (when (or (string= (buffer-name) "*scratch*")
-            (eq (current-buffer) (get-buffer "*scratch*")))
-    (zmacs-dashboard)
-    (zmacs-goto-dashboard)))
-
-;; Hook into emacsclient:
-(add-hook 'server-after-make-frame-hook #'zmacs/new-frame-file-or-dashboard)
+;;;; Utilities:
 
 (defun zmacs//terminal-is-initial (terminal)
   "Return T if TERMINAL is the initial terminal."
   (string= (terminal-name terminal) "initial_terminal"))
+
+(defun zmacs//frame-in-initial (frame)
+  "Return T if FRAME is parented to the initial terminal."
+  (let ((parent (frame-parent frame)))
+    (if (not (null parent))
+        (zmacs//frame-in-initial parent)
+      (zmacs//terminal-is-initial (frame-terminal frame)))))
+
+(defun zmacs//frame-in-terminal (frame)
+  "Return T if FRAME is in a terminal."
+  (let ((parent   (frame-parent frame)))
+    (if (not (null parent))
+        (zmacs//frame-in-terminal parent)
+      (let* ((terminal (frame-terminal frame))
+             (live     (terminal-live-p terminal)))
+        (cond ((zmacs//terminal-is-initial terminal) t)
+              ((equal live 't)                       t)
+              (t                                     nil))))))
 
 (defun zmacs/server-client-count ()
   "Return the number of emacsclient connections."
@@ -84,8 +91,45 @@
                      (cl-remove-if #'zmacs//terminal-is-initial
                                    (terminal-list)))))
 
-;;; Provide package:
+(defun zmacs/frame-count ()
+  "Return the number of frames."
+  (length (cl-remove-if (lambda (frame)
+                          (or (zmacs//frame-in-initial frame)
+                              (not (null (frame-parent frame)))))
+                        (frame-list))))
 
-(provide 'zmacs-server)
+(defun zmacs/server-gui-frame-count ()
+  "Return the number of GUI frames that the Emacs server has.
+
+This will ignore any frame that has a parent, or any frame that is in a
+CLI Emacs session."
+  (length
+   (cl-remove-if (lambda (frame)
+                   (or (zmacs//frame-in-terminal frame)
+                       (not (null (frame-parent frame)))))
+                 (frame-list))))
+
+;;;; New frame:
+
+(defun zmacs/new-frame-file-or-dashboard ()
+  "Show dashboard if FRAME is `*scratch*'.
+
+If the new frame is the only frame, and there is no filename given, then
+Emacs will treat this similarly to a plain client.  It will open using as
+much as the screen as configured, and will switch to the ZMACS dashboard.
+
+If it is either not the first frame, or a filename was given, then it will
+be treated as a plain `emacsclient' frame."
+  (if (= (zmacs/server-gui-frame-count) 1)
+      (zlisp/initial-frame-size)
+    (zlisp/initial-server-frame-size))
+  (zmacs-line-clockface-update-fontset "ClockFaceRectSolid")
+  (when (or (string= (buffer-name) "*scratch*")
+            (eq (current-buffer) (get-buffer "*scratch*")))
+    (zmacs-dashboard)
+    (zmacs-goto-dashboard)))
+
+;; Hook into emacsclient:
+(add-hook 'server-after-make-frame-hook #'zmacs/new-frame-file-or-dashboard)
 
 ;;; zmacs-server.el ends here.
